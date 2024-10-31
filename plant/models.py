0cf48293b43a1a63bfa3418c921ae62025288344
My name <you44@example.com>
2024-10-31 16:02:19 +0200

    add module plant and create Plant model

---
+++ plant/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Plant(models.Model):
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    watering_frequency_days = models.IntegerField()
+    last_watered_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-last_watered_date']
